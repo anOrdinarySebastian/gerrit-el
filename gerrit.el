ceb46435b0453f9f04c1c4aec9c8315733ad054a
Sebastian Nirvin <sebe@hms.se>
2022-12-09 16:04:39 +0100

    Refactored file into functions
    Also dug deeper into the json structure

--- gerrit.el
+++ gerrit.el
@@ -2,38 +2,43 @@
 
 ;; Author: Sebastian Nirvin
 
-;; (setq explicit-shell-file-name "c:/Program Files/Git/bin/bash.exe")
-;; (setq shell-file-name "bash")
+(message "Loading gerrit.el...")
 
-(let ((process-name "gerrit-get")
-      (process-connection-type nil)
-      (buf-name "*gerrit log*"))
+(defun get-gerrit-comments ()
+  "Main entry point"
+  (interactive)
+  "Function for fetching gerrit comments from the latest log"
+  (gerrit-getter--request-comments "I2be2a5c392830a1d3ddd84aa8ac592ed0c4101e1")
+  (gerrit-getter--parse-comment-positions)
+  )
 
-  (kill-buffer buf-name)
-  (get-buffer-create buf-name)
+(setq gerrit-getter-response-buffer-name "*gerrit log*")
 
-  ;; (start-process process-name buf-name "ssh"
-  ;;                "-p" "29418"
-  ;;                "review.hms.se"
-  ;;                "gerrit"
-  ;;                "query"
-  ;;                "--patch-sets"
-  ;;                "--format=JSON"
-  ;;                "--comments"
-  ;;                "change:Idb19e485226a80fcca730d9b1b80b84be20a77bd")
+(defun gerrit-getter--request-comments (change)
+  "Fetch the comments from Gerrit with an SSH command"
 
-  (call-process "ssh" nil buf-name nil
-                "-p 29418"
-                "review.hms.se"
-                "gerrit"
-                "query"
-                "--patch-sets"
-                "--format=JSON"
-                "--comments"
-                "change:I2be2a5c392830a1d3ddd84aa8ac592ed0c4101e1"
-                )
+  (let ((process-name "gerrit-get")
+        (process-connection-type nil))
 
-  (with-current-buffer buf-name
+    (ignore-error error (kill-buffer gerrit-getter-response-buffer-name))
+    (get-buffer-create gerrit-getter-response-buffer-name)
+    (call-process "ssh" nil gerrit-getter-response-buffer-name nil
+                  "-p 29418"
+                  "review.hms.se"
+                  "gerrit"
+                  "query"
+                  "--patch-sets"
+                  "--format=JSON"
+                  "--comments"
+                  (format "change: %s" change)
+                  )
+    )
+  )
+
+(defun gerrit-getter--parse-comment-positions ()
+  "Function for extracting the position of the comments"
+
+  (with-current-buffer gerrit-getter-response-buffer-name
     ;; TODO
     ;; - Figure out what values to pick out
     ;; - Figure out how to put them in a buffer, take insperation from somewhere?
@@ -42,7 +47,16 @@
     (delete-region (+ (search-backward "}") 1) (point-max))
     (goto-char (point-min))
     (let ((returned-json (json-parse-buffer :object-type 'alist)))
+
       (message (alist-get 'project returned-json))
+      (message "The number is %s"
+               (alist-get 'number
+                          (elt
+                           (reverse
+                            (alist-get 'patchSets returned-json)) 0 )))
+      ;; (message (alist-get 'comments (alist-get 'patchSets)))
       )
     )
   )
+
+(message "Done!")
